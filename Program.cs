e89fe6f748687c97f687b7d016bb097b84f5a935
ThapeloCode <thplhpp@gmail.com>
2026-04-05 14:53:04 +0200

    Initial-Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using IceTaskOne;
+
+string Name;
+int Marks;
+
+Student student; 
+Dictionary<string, int> students = new Dictionary<string, int>();
+
+Console.WriteLine("STUDENT DETAILS");
+//PROMPT NAME
+Console.WriteLine("NAME     :   ");
+Name = Console.ReadLine();
+
+//PROMPT MARKS
+Console.WriteLine("MARKS    :   ");
+Marks = Convert.ToInt32(Console.ReadLine());
+
+//ADD STUDENT TO DICTIONARY
+student = new Student(Name, Marks);
+students.Add(student.GetName(), student.GetMarks());
+
+//RETURN ALL STUDENTS
+student.GetResults(students);
---
+++ Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace IceTaskOne
+{
+
+    internal class Student
+    {
+        private string Name;
+        private int Marks;
+        Dictionary<string, int> students;
+
+
+
+        public Student(string Name, int Marks)
+        { 
+            this.Name = Name;
+            this.Marks = Marks;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public int GetMarks()
+        { 
+            return Marks;
+        }
+
+        public void GetResults(Dictionary<string, int> students)
+        {
+            foreach (var (Name, Marks) in students)
+            {
+                Console.WriteLine($"{Name}: {Marks}");
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
